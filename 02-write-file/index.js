9019da32ced4a93bf3c9cffea87e8304a10ded94
Mikhail <mihkailmatveichuk@gmail.com>
2022-10-29 19:26:41 +0300

    fead: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+// const process = require('process');
+var path = require('path');
+const fs = require('fs');
+const { stdin, stdout } = process;
+const keyWord = 'exit';
+
+fs.open(path.join(__dirname, '../02-write-file/file.txt'), 'w', (err) => {
+  if(err) throw err;
+  stdout.write('Введите текст!\n');
+    process.on('SIGINT', ()=>{
+      console.log('Удачи в изучении Node.js!');
+      process.exit();
+    });
+  });
+
+    stdin.on('data', key => {
+      fs.appendFile(path.join(__dirname, '../02-write-file/file.txt'), key, (err) => {
+        if(err) throw err;
+    });
+  });
+
+  process.stdin.on('data', function (wordKey) {
+    if(wordKey.toString().toLowerCase().trim() === 'exit'){
+      console.log('Удачи в изучении Node.js!');
+      process.exit();
+    }
+});
+
