1295604839813924b4bdeac5868d920bf6fec79f
Mikhail <mihkailmatveichuk@gmail.com>
2022-10-31 18:15:24 +0300

    refactor: fix some mistakes

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const { readdir } = require('fs/promises');
 const path = require('path');
-const neededAdress = path.join(__dirname, '../03-files-in-folder/secret-folder');
+const neededAddress = path.join(__dirname, '../03-files-in-folder/secret-folder');
 
-readdir(neededAdress, { withFileTypes: true }).then(data => data.forEach(file => {
+readdir(neededAddress, { withFileTypes: true }).then(data => data.forEach(file => {
     if (file.isFile()) {
-        fs.stat(path.join(neededAdress, file.name), (error, stats) => {
+        fs.stat(path.join(neededAddress, file.name), (error, stats) => {
             if (error) {
                 console.log(error);
             } else {
                 const name = file.name.split('.')[0];
-                const extansion = file.name.split('.')[1];
+                const extension = file.name.split('.')[1];
                 const size = (stats.size / 1024).toFixed(3);
-                return console.log(`${name} - ${extansion} - ${size}kb`);
+                return console.log(`${name} - ${extension} - ${size}kb`);
             }
         });
     }
