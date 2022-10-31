de35a87fc5c96769ca3d83c018793398845f21f9
mihailmatveichuk <mihkailmatveichuk@gmail.com>
2022-10-31 12:09:21 +0300

    fix: change solution of 03-files-in-folder task using promise

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,24 +1,27 @@
 const fs = require('fs');
+const { readdir } = require('fs/promises');
 const path = require('path');
 const neededAdress = path.join(__dirname, '../03-files-in-folder/secret-folder');
 
-fs.readdir(neededAdress, { withFileTypes: true }, (err, files) => {
-    if (err) console.log(err);
-    else {
-        console.log("Current directory filenames:");
-        files.forEach(file => {
-            if (file.isFile()) {
-                fs.stat(path.join(neededAdress, file.name), (error, stats) => {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        const name = file.name.split('.')[0];
-                        const extansion = file.name.split('.')[1];
-                        const size = (stats.size / 1024).toFixed(3);
-                        return console.log(`${name} - ${extansion} - ${size}kb`);
-                    }
-                });
+readdir(neededAdress, { withFileTypes: true }).then(data => data.forEach(file => {
+    if (file.isFile()) {
+        fs.stat(path.join(neededAdress, file.name), (error, stats) => {
+            if (error) {
+                console.log(error);
+            } else {
+                const name = file.name.split('.')[0];
+                const extansion = file.name.split('.')[1];
+                const size = (stats.size / 1024).toFixed(3);
+                return console.log(`${name} - ${extansion} - ${size}kb`);
             }
         });
     }
-});
+}));
+//     if (err) console.log(err);
+//     else {
+//         console.log("Current directory filenames:");
+//         files.forEach(file => {
+
+//         });
+//     }
+// });
