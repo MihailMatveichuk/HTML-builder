204b95c37477474d1a54f9b81e42a97f9f08a67d
Mikhail <mihkailmatveichuk@gmail.com>
2022-10-31 21:27:39 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const directFolder = __dirname + '\\files';
 const directCopy = __dirname + '\\files-copy';
+// const wayCurrent = path.join(directFolder);
+// const wayCopy = path.join(directCopy);
 
-function copyDir() {
+function checkExist(){
     fs.access(path.join(directCopy), (err) => {
         if (err) {
             fs.mkdir(path.join(directCopy), { recursive: true }, (err) => {
@@ -12,19 +14,26 @@ function copyDir() {
                 }
                 console.log('Directory created successfully!');
             });
-            copyDir(directFolder, directCopy);
+            copyDir();
         } else {
             console.log('This directory has already exists');
         }
     });
-    fs.copyFile(path.join(directFolder), path.join(directCopy),
-        fs.constants.COPYFILE_EXCL, (err) => {
-            if (err) {
-                console.log("Error Found:", err);
-            } else {
-                console.log("\nFile Contents of copied_file:");
-            }
-        });
 }
 
-copyDir();
+function copyDir() {
+    fs.readdir(path.join(directFolder), { withFileTypes: true }, (err, files) => {
+        if (err) console.log(err);
+        else {
+          files.forEach(file => {
+            fs.copyFile(path.join(directFolder, file.name), path.join(directCopy, file.name), (err)=>{
+                if(err){
+                    console.log(err);
+                }
+            });
+          });
+        }
+      });
+}
+
+checkExist();
