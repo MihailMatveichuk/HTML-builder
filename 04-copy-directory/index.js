4a78258c1732d514f2e80328afcbdc4201a1298f
mihailmatveichuk <mihkailmatveichuk@gmail.com>
2022-10-31 14:52:01 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const directFolder = __dirname + '\\files';
+const directCopy = __dirname + '\\files-copy';
 
+function copyDir() {
+    fs.access(path.join(directCopy), (err) => {
+        if (err) {
+            fs.mkdir(path.join(directCopy), { recursive: true }, (err) => {
+                if (err) {
+                    return console.error(err);
+                }
+                console.log('Directory created successfully!');
+            });
+            copyDir(directFolder, directCopy);
+        } else {
+            console.log('This directory has already exists');
+        }
+    });
+    fs.copyFile(path.join(directFolder), path.join(directCopy),
+        fs.constants.COPYFILE_EXCL, (err) => {
+            if (err) {
+                console.log("Error Found:", err);
+            } else {
+                console.log("\nFile Contents of copied_file:");
+            }
+        });
+}
+
+copyDir();
